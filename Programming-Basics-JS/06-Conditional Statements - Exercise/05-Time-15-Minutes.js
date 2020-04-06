5ed1698cfb7537a04b75a3777039a3622adfb150
Ellie Georgieva <63105567+Ellie-Georgieva@users.noreply.github.com>
2020-04-07 00:04:01 +0300

    Update 05-Time-15-Minutes

--- Programming-Basics-JS/06-Conditional Statements - Exercise/05-Time-15-Minutes.js
+++ Programming-Basics-JS/06-Conditional Statements - Exercise/05-Time-15-Minutes.js
@@ -1,3 +1,32 @@
+//short version
+
+function time15Minutes(hours, minutes) {
+    hours = Number(hours);
+    minutes = Number(minutes);
+
+    let totalMinutes = hours * 60 + minutes + 15;
+    let finalHours = Math.floor(totalMinutes / 60);
+    let finalMinutes = totalMinutes % 60;
+
+    if (finalHours > 24) {
+        finalHours = 0;
+    }
+
+    if (finalMinutes < 10) {
+        console.log(`${hour}:0${min}`);
+    } else {
+        console.log(`${hour}:${min}`);
+    }
+}
+
+time15Minutes(1, 46);
+time15Minutes(0, 01);
+time15Minutes(23, 59);
+time15Minutes(11, 08);
+time15Minutes(12, 49);
+
+//long version
+
 function time15Minutes(hours, minutes) {
     hours = Number(hours);
     minutes = Number(minutes);
