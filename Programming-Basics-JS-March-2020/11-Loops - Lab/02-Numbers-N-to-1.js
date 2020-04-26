a2e560bb4bbab44c08fef3734b3ce3c119a44264
Ellie Georgieva <63105567+Ellie-Georgieva@users.noreply.github.com>
2020-04-26 11:51:54 +0300

    Create 02-Numbers-N-to-1.js

---
+++ Programming-Basics-JS-March-2020/11-Loops - Lab/02-Numbers-N-to-1.js
@@ -0,0 +1,11 @@
+function numbersFromNto1(n) {
+    n = Number(n);
+
+    for (let i = n; i >= 1; i--) {
+        console.log(i);
+    }
+}
+
+numbersFromNto1(2);
+numbersFromNto1(3);
+numbersFromNto1(5);
