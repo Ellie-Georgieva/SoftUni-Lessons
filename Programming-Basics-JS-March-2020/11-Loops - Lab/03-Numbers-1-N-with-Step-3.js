87e124e1885fb87504e095932b352c919a1b351a
Ellie Georgieva <63105567+Ellie-Georgieva@users.noreply.github.com>
2020-04-27 10:43:31 +0300

    Create 03-Numbers-1-N-with-Step-3.js

---
+++ Programming-Basics-JS-March-2020/11-Loops - Lab/03-Numbers-1-N-with-Step-3.js
@@ -0,0 +1,11 @@
+function numbers1toNwithStep3(n) {
+    n = Number(n);
+
+    for (let i = 1; i <= n; i += 3) {
+        console.log(i);
+    }
+}
+
+numbers1toNwithStep3(10);
+numbers1toNwithStep3(7);
+numbers1toNwithStep3(15);
