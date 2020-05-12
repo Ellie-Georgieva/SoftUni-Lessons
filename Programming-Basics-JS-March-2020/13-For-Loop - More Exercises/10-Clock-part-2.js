88f629fd3059f8864d62dad55aab9d2df013612c
Ellie Georgieva <63105567+Ellie-Georgieva@users.noreply.github.com>
2020-05-12 10:25:05 +0300

    Create 10-Clock-part-2.js

---
+++ Programming-Basics-JS-March-2020/13-For-Loop - More Exercises/10-Clock-part-2.js
@@ -0,0 +1,12 @@
+function clockPart2() {
+
+    for (let hours = 0; hours <= 23; hours++) {
+        for (let minutes = 0; minutes <= 59; minutes++) {
+            for (let seconds = 0; seconds <= 59; seconds++) {
+                console.log(`${hours} : ${minutes} : ${seconds}`);
+            }
+        }
+    }
+}
+
+clockPart2();
