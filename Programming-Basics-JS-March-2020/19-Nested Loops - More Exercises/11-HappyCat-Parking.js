aa29d39db0b812ed20c310e84cb817881e53ae21
Ellie Georgieva <63105567+Ellie-Georgieva@users.noreply.github.com>
2020-06-03 09:48:13 +0300

    Create 11-HappyCat-Parking.js

---
+++ Programming-Basics-JS-March-2020/19-Nested Loops - More Exercises/11-HappyCat-Parking.js
@@ -0,0 +1,28 @@
+function happyCatParking(arr) {
+    let days = Number(arr.shift());
+    let hours = Number(arr.shift());
+    let totalPrice = 0;
+
+    for (let d = 1; d <= days; d++) {
+        let price = 0;
+
+        for (let h = 1; h <= hours; h++) {
+
+            if (d % 2 == 0 && h % 2 != 0) {
+                price += 2.5;
+            } else if (d % 2 != 0 && h % 2 == 0) {
+                price += 1.25;
+            } else {
+                price += 1;
+            }
+        }
+
+        console.log(`Day: ${d} - ${price.toFixed(2)} leva`);
+        totalPrice += price;
+    }
+
+    console.log(`Total: ${totalPrice.toFixed(2)} leva`);
+}
+
+happyCatParking([2, 5]);
+happyCatParking([5, 2]);
