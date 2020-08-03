4ef77f841fc7b08f9621320bd988f18e24cc1b12
Ellie Georgieva <63105567+Ellie-Georgieva@users.noreply.github.com>
2020-08-03 14:59:39 +0300

    Create 01-Multiply-Number-by-2.js

---
+++ Fundamentals -JS-2020/01-Basic Syntax, Conditional Statements and Loops - Lab/01-Multiply-Number-by-2.js
@@ -0,0 +1,10 @@
+function multiplyNumberBy2(numInput) {
+    const num = Number(numInput);
+    const result = num * 2;
+
+    console.log(result);
+}
+
+multiplyNumberBy2(2);
+multiplyNumberBy2(5);
+multiplyNumberBy2(20);
