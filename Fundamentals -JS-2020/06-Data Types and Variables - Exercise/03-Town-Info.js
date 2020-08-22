c09f71e240ae245fd40aebb878f986fd55555ac6
Ellie Georgieva <63105567+Ellie-Georgieva@users.noreply.github.com>
2020-08-22 09:50:26 +0300

    Create 03-Town-Info.js

---
+++ Fundamentals -JS-2020/06-Data Types and Variables - Exercise/03-Town-Info.js
@@ -0,0 +1,9 @@
+function townInfo(towninput, populationInput, areaInput) {
+    const town = towninput;
+    const population = populationInput;
+    const area = areaInput;
+
+    console.log(`Town ${town} has population of ${population} and area ${area} square km.`);
+}
+
+townInfo('Sofia', 1286383, 492);
