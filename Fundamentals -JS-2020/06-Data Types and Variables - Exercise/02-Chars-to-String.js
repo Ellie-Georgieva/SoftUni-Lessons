20b5a05a8ec3d12821695e70d32e71ca56483ceb
Ellie Georgieva <63105567+Ellie-Georgieva@users.noreply.github.com>
2020-08-22 09:49:40 +0300

    Create 02-Chars-to-String.js

---
+++ Fundamentals -JS-2020/06-Data Types and Variables - Exercise/02-Chars-to-String.js
@@ -0,0 +1,12 @@
+function charsToString(firstCharInput, secondCharInput, thirdCharInput) {
+    const firstChar = firstCharInput;
+    const secondChar = secondCharInput;
+    const thirdChar = thirdCharInput;
+
+    const result = firstChar + secondChar + thirdChar;
+    console.log(result);
+}
+
+charsToString('a', 'b', 'c');
+charsToString('%', '2', 'o');
+charsToString('1', '5', 'p');
