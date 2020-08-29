4fde5be0dcedb617206312c51679b467cac8b916
Ellie Georgieva <63105567+Ellie-Georgieva@users.noreply.github.com>
2020-08-29 09:21:33 +0300

    Create 05-Binary-to-Decimal.js

---
+++ Fundamentals -JS-2020/07-Data Types and Variables - More Exercise/05-Binary-to-Decimal.js
@@ -0,0 +1,8 @@
+function binaryToDecimal(binaryNumInput) {
+    let binaryNum = binaryNumInput;
+
+    console.log(parseInt(binaryNum, 2));
+}
+
+binaryToDecimal(00001001);
+binaryToDecimal(11110000);
